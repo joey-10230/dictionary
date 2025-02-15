774855a316996f991a1fee2e7af6cce583e047fb
joey-10230 <parayaoanjoey874@gmail.com>
2025-02-15 22:30:44 +0800

    Add files via upload
    FRUIT DICTIONARY

---
+++ newfile.py
@@ -0,0 +1,37 @@
+print("Welcome to fruit Dictionary")
+
+data= {
+"Apple":"A round fruit with smooth skin, usually red, green, or yellow, with a sweet or tart taste.",
+"Banana":"A long curved fruit with yellow skin when ripe, soft, sweet flesh, and rich in potassium.",
+"Mango":"A tropical fruit with smooth skin, which can be green, yellow or red, and has sweet or slightly sour flesh.",
+"Water melon":"A large fruit with a thick green rind juicy red or pink flesh, and black seeds, known for its high water content.",
+"Orange":"A citrus fruit with a thick, bright orange peel and juicy, tangy sweet segments inside.",
+"Grapes":"Small round fruits that grow in clusters available in green, red or purple, often eaten fresh or used for making wine.",
+"Strawberry":"A small red heart shaped fruit with tiny seeds on its surface.",
+"Pineapple":"A tropical with rough, spiky skin and sweet, juicy yellow flesh inside.",
+"Cherry":"A small round fruit that is usually red or dark purple, with a sweet or tart taste.",
+"Papaya":"A tropical fruit with orange flesh black seeds and soft sweet texture when ripe.",
+"pear":"A bell-shaped with smooth or slightly grainy skull.",
+"Peach":"A round fruit with fuzzy skin.",
+"Plum":"A small round fruit with smooth skin.",
+"Lemon":" A yellow citrus fruit with sour taste",
+"Lime":"A small green citrus fruit with sour taste.",
+"Avocado":"A creamy green fruit with large seed.",
+"Kiwi":"A small brown fruit with fuzzy skin.",
+"Blue Berry":"A small round blue or purple fruit with sweet and slightly tart flavor.",
+"Raspberry":"A small red or black berry witg delicate, sweet tart taste.",
+"Black berry":"A dark purple or black fruit with a slightly sweet and tart taste.",
+"Pomegrenate":"A round fruit with a tough red rind.",
+"Coconut":"A tropical fruit with a hard and hairy shell.",
+"Fig":"A small pear shaped fruit with soft sweet flesh nd tiny edible seed.",
+"Dragon fruit":"A vibrant pink or yellow tropical fruit with spiky skin.",
+"Guava":"A tropical fruit with green or yellow skin.",
+"Cantaloupe":"A mellon with  rough, netted rind and sweet orange flesh inside.",
+"Honey due":" A smooth pale green melon with sweet juicy.",
+"Cranberry":"A small red, tart fruit wutg spiky skin and sweet, yellow flesh inside.",
+"Jack fruit":"A large tropical fruit with spiky skin and sweet yellow flesh inside.",
+"Tamarind":"A brown pod like fruit with a tangy sweet pulp.",
+}
+
+a=input("Welcome to fruit dictionary")
+print(data[a])
